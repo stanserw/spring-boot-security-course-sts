ffbb6ed95a20053e82118756fb52eabb4814755d
Stanisław Serwin <stanserw@wp.pl>
2022-03-06 16:31:46 +0100

    czwarty branch, uprawnienia i autentyfikacja

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,50 +14,66 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.security.ApplicationUserRole.ADMIN;
-import static com.example.demo.security.ApplicationUserRole.STUDENT;
+import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
+  private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+  @Autowired
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/","index","/css/*", "/js.*").permitAll()
-                .antMatchers("/api/**").hasRole(STUDENT.name())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .csrf().disable()// TODO: I will teach this in detail in the next section
+        .authorizeRequests()
+        .antMatchers("/", "index", "/css/*", "/js.*").permitAll()
+        .antMatchers("/api/**").hasRole(STUDENT.name())
+//        .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//        .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//        .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//        .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
 
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails annaSmithUser = User.builder()
-                .username("annasmith")
-                .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name()) // ROLE_STUDENT
-                .build();
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails annaSmithUser = User.builder()
+        .username("annasmith")
+        .password(passwordEncoder.encode("password"))
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+        .authorities(STUDENT.grantedAuthorities())
+        .build();
 
-        UserDetails lindaUser = User.builder()
-                .username("linda")
-                .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
-                .build();
+    UserDetails lindaUser = User.builder()
+        .username("linda")
+        .password(passwordEncoder.encode("password123"))
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+        .authorities(ADMIN.grantedAuthorities())
+        .build();
 
-        return new InMemoryUserDetailsManager(
-                annaSmithUser,
-                lindaUser
-        );
-    }
+    UserDetails tomUser = User.builder()
+        .username("tom")
+        .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+        .authorities(ADMINTRAINEE.grantedAuthorities())
+        .build();
+
+    return new InMemoryUserDetailsManager(
+        annaSmithUser,
+        lindaUser,
+        tomUser
+    );
+  }
 }
